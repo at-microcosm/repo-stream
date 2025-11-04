e14c310fbd7cfec077dd8a8d15a40b9d040d1233
phil <uniphil@gmail.com>
2025-11-04 18:23:38 -0500

    files cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,6 @@
 //! For now see the [examples](https://tangled.org/@microcosm.blue/repo-stream/tree/main/examples)
 
 pub mod disk;
-pub mod disk_drive;
-pub mod disk_redb;
-pub mod disk_sqlite;
-pub mod disk_walk;
 pub mod drive;
 pub mod mst;
 pub mod walk;
